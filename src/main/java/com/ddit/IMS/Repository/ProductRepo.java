66f63eccf763a7f06d5e075f6b2ffc85ec6ca2f0
21CEUOS101 <21ceuos101@ddu.ac.in>
2023-06-24 17:32:01 +0530

    First Version

--- src/main/java/com/ddit/IMS/Repository/ProductRepo.java
+++ src/main/java/com/ddit/IMS/Repository/ProductRepo.java
@@ -1,7 +1,6 @@
 package com.ddit.IMS.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ddit.IMS.Models.Product;
